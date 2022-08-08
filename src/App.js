67b5fa02b49626751771984f01ef5dc990f37087
CVSCharan <charan.cvs@gmail.com>
2022-08-09 02:36:41 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -18,35 +18,52 @@ class App extends Component {
   click4 = () => {
     this.setState({ color: "colorbb6bd9" });
   };
+
   render() {
     const { color } = this.state;
     const setColor = `d-flex flex-column justify-content-center ${color}`;
     return (
-      <div id="colorPickerContainer" class={setColor}>
-        <h1 class="heading1">Color Picker</h1>
-        <div class="card-container">
-          <button class="button button-1" id="button1" onClick={this.click1}>
+      <div id="colorPickerContainer" className={setColor}>
+        <h1 className="heading1">Color Picker</h1>
+        <div className="card-container">
+          <button
+            className="button button-1"
+            id="button1"
+            onClick={this.click1}
+          >
             #e0e0e0
           </button>
-          <button class="button button-2" id="button2" onClick={this.click2}>
+          <button
+            className="button button-2"
+            id="button2"
+            onClick={this.click2}
+          >
             #6fcf97
           </button>
-          <button class="button button-3" id="button3" onClick={this.click3}>
+          <button
+            className="button button-3"
+            id="button3"
+            onClick={this.click3}
+          >
             #56ccf2
           </button>
-          <button class="button button-4" id="button4" onClick={this.click4}>
+          <button
+            className="button button-4"
+            id="button4"
+            onClick={this.click4}
+          >
             #bb6bd9
           </button>
         </div>
-        <div class="para-container">
-          <p class="bg-para">
+        <div className="para-container">
+          <p className="bg-para">
             Background Color :{" "}
-            <span class="span1" id="selectedColorHexCode">
+            <span className="span1" id="selectedColorHexCode">
               {color.slice(5, color.length)}
             </span>
           </p>
         </div>
-        <p class="para1">
+        <p className="para1">
           Try clicking on one of the colors above to change the background color
           of this page!
         </p>
